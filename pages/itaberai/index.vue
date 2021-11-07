a88785f6ea3ca19d2f2a07ab31e3ac39fe647814
Alexandre Calil <alexandrecalilmf@gmail.com>
2021-11-07 12:12:55 -0300

    chore: add itaberai routes

---
+++ pages/itaberai/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<section class="home">
+		<h1>Hello from Itaberaí</h1>
+	</section>
+</template>
+
+<script>
+	export default {
+		name: 'Home',
+		head() {
+			return {
+				title: 'Home',
+			};
+		},
+	};
+</script>
