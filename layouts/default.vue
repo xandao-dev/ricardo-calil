4cea693606a16c1bd279edd78e72b2f57c6b6e6a
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-01-29 13:03:20 -0300

    fix: mobile sidebar overflow

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-	<div :class="{ 'overflow-hidden': isSidebarOpen }">
+	<div>
 		<header class="sticky top-0 z-50">
 			<Navbar></Navbar>
 		</header>
@@ -16,33 +16,40 @@
 </template>
 
 <script>
-	import { mapState } from 'vuex';
-	export default {
-		data() {
-			return {
-				social: {
-					whatsapp: '556233752216',
-					facebook: 'ricardocaliladv',
-					instagram: 'dr.ricardocalil',
-					phone: '+55 62 3375-2216',
-				},
-				icons: {
-					menu: '/icons/menu.svg',
-					menuOpen: '/icons/menu-open.svg',
-					phone: '/icons/phone.svg',
-					whatsapp: '/icons/whatsapp.svg',
-					facebook: '/icons/facebook.svg',
-					instagram: '/icons/instagram.svg',
-					linkedin: '/icons/linkedin.svg',
-					location: '/icons/location.svg',
-					map: '/icons/map.svg',
-				},
-			};
-		},
-		computed: {
-			...mapState('Hamburger', {
-				isSidebarOpen: 'isSidebarOpen',
-			}),
-		},
-	};
+import { mapState } from 'vuex';
+export default {
+	data() {
+		return {
+			social: {
+				whatsapp: '556233752216',
+				facebook: 'ricardocaliladv',
+				instagram: 'dr.ricardocalil',
+				phone: '+55 62 3375-2216',
+			},
+			icons: {
+				menu: '/icons/menu.svg',
+				menuOpen: '/icons/menu-open.svg',
+				phone: '/icons/phone.svg',
+				whatsapp: '/icons/whatsapp.svg',
+				facebook: '/icons/facebook.svg',
+				instagram: '/icons/instagram.svg',
+				linkedin: '/icons/linkedin.svg',
+				location: '/icons/location.svg',
+				map: '/icons/map.svg',
+			},
+		};
+	},
+	head() {
+		return {
+			bodyAttrs: {
+				class: this.isSidebarOpen ? 'overflow-hidden' : '',
+			},
+		};
+	},
+	computed: {
+		...mapState('Hamburger', {
+			isSidebarOpen: 'isSidebarOpen',
+		}),
+	},
+};
 </script>
