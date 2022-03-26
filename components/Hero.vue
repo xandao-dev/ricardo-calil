17dc40cdfd88606409a9ae5019f19d8942bae9d2
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-03-26 14:46:14 -0300

    chore: add hero for sm

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,16 +3,16 @@
 		class="
 			before:absolute before:inset-0 before:h-full before:w-full
 			hero-bg
-			p-6
-			sm:py-12
+			px-8
+			py-12
 			lg:py-24 lg:flex-row lg:justify-between
 		"
 	>
-		<div class="relative flex flex-col justify-center px-3 py-6 text-center lg:max-w-md xl:max-w-lg lg:text-left">
-			<h1 class="text-4xl font-bold text-white leading-none sm:text-6xl">
+		<div class="relative flex flex-col justify-center text-center lg:max-w-md xl:max-w-lg lg:text-left">
+			<h1 class="text-4xl font-bold text-white sm:text-6xl">
 				30 anos de <span class="text-sky-600">advocacia</span> para te ajudar
 			</h1>
-			<p class="text-gray-200 my-6 text-md sm:mb-12">
+			<p class="text-gray-200 mt-6 mb-12 sm:px-6">
 				Somos altamente qualificados <br class="hidden md:inline lg:hidden" />e estamos prontos para resolver
 				seus problemas de maneira eficiente e simplificada.
 			</p>
@@ -24,13 +24,11 @@
 					focus:ring-4 focus:ring-blue-300
 					font-semibold
 					rounded
-					text-md
 					px-6
 					py-3
 					mx-auto
-					dark:bg-blue-600 dark:hover:bg-blue-700
 					focus:outline-none
-					dark:focus:ring-blue-800
+					sm:px-8 sm:py-4 sm:text-xl
 				"
 			>
 				Fale agora com um advogado
@@ -39,7 +37,7 @@
 		<img
 			src="/hero/hero.webp"
 			alt="hero-escritorio"
-			class="hidden object-contain z-0 lg:mt-0 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+			class="hidden object-contain lg:mt-0 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
 		/>
 	</section>
 </template>
