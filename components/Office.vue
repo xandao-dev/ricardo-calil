d9ac0b23ce01288654f162f6b98ec294f4a7e347
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-03-27 13:57:52 -0300

    chore: improve office html

--- components/Office.vue
+++ components/Office.vue
@@ -1,97 +1,36 @@
 <template>
-	<section class="px-8 py-12 grid gap-4 mx-4 sm:grid-cols-12 lg:px-12 xl:px-16">
-		<div
-			class="
-				col-span-12
-				text-center
-				sm:text-left sm:col-span-3
-				mb-14
-				before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto
-				sm:before:mx-0
-			"
-		>
+	<section class="px-8 py-12 grid sm:grid-cols-12 lg:px-12 xl:px-16">
+		<div class="col-span-12 text-center sm:text-left sm:col-span-4 mb-12">
 			<h1 class="text-4xl font-bold text-black">Escritório</h1>
 			<span class="text-sm font-bold tracking-wider uppercase">Conheça nossa história</span>
 		</div>
-		<div class="relative col-span-12 px-4 space-y-6 sm:col-span-9">
-			<div
-				class="
-					col-span-12
-					space-y-12
-					relative
-					px-4
-					sm:col-span-8
-					sm:space-y-8
-					sm:before:absolute
-					sm:before:top-2
-					sm:before:bottom-0
-					sm:before:w-0.5
-					sm:before:-left-3
-				"
-			>
-				<div
-					class="
-						flex flex-col
-						sm:relative
-						sm:before:absolute
-						sm:before:top-2
-						sm:before:w-4
-						sm:before:h-4
-						sm:before:rounded-full
-						sm:before:left-[-35px]
-						sm:before:z-[1]
-					"
-				>
-					<h3 class="text-xl font-semibold tracking-wide">Fundação</h3>
-					<time class="text-xs tracking-wide uppercase text-coolGray-600">fev 1992</time>
-					<p class="mt-3">
-						Fundado em 1992, pelo advogado Dr. Ricardo Calil Fonseca, o escritório de advocacia atua em
-						diversos segmentos, com foco em causas trabalhistas e previdenciárias. Com sede em Itaberaí, mas
-						atendendo em diversas cidades como a cidade de Goiás, Mossâmedes, Itapuranga, Heitoraí,
-						Itaguaru, Inhumas e Goiânia.
-					</p>
-				</div>
-				<div
-					class="
-						flex flex-col
-						sm:relative
-						sm:before:absolute
-						sm:before:top-2
-						sm:before:w-4
-						sm:before:h-4
-						sm:before:rounded-full
-						sm:before:left-[-35px]
-						sm:before:z-[1]
-					"
-				>
-					<h3 class="text-xl font-semibold tracking-wide">Associação</h3>
-					<time class="text-xs tracking-wide uppercase text-coolGray-600">jan 2015</time>
-					<p class="mt-3">
-						Em janeiro de 2015 iniciou-se a carreira do Dr. Lucilo Fonseca Neto no escritório, atuando ao
-						lado de seu pai, o advogado Dr.Ricardo Calil.
-					</p>
-				</div>
-				<div
-					class="
-						flex flex-col
-						sm:relative
-						sm:before:absolute
-						sm:before:top-2
-						sm:before:w-4
-						sm:before:h-4
-						sm:before:rounded-full
-						sm:before:left-[-35px]
-						sm:before:z-[1]
-					"
-				>
-					<h3 class="text-xl font-semibold tracking-wide">Conquistas</h3>
-					<time class="text-xs tracking-wide uppercase">Jan 2016</time>
-					<p class="mt-3">
-						Durante a caminhada de Dr. Ricardo Calil na advocacia, recebeu a honrosa missão de ser o
-						presidente a OAB subseção de Itaberaí, no período de 2016 a 2018. Uma conquista que lhe trouxe
-						muitas experiências.
-					</p>
-				</div>
+		<div class="col-span-12 space-y-8 px-2 sm:col-span-8">
+			<div class="flex flex-col">
+				<h3 class="text-xl font-semibold tracking-wide">Origem</h3>
+				<time class="text-xs tracking-wide uppercase text-coolGray-600">fev 1992</time>
+				<p class="mt-3">
+					Fundado em 1992, pelo advogado Dr. Ricardo Calil Fonseca, o escritório de advocacia atua em diversos
+					segmentos, com foco em causas trabalhistas e previdenciárias. Com sede em Itaberaí, mas atendendo em
+					diversas cidades como a cidade de Goiás, Mossâmedes, Itapuranga, Heitoraí, Itaguaru, Inhumas e
+					Goiânia.
+				</p>
+			</div>
+			<div class="flex flex-col">
+				<h3 class="text-xl font-semibold tracking-wide">Associação</h3>
+				<time class="text-xs tracking-wide uppercase text-coolGray-600">jan 2015</time>
+				<p class="mt-3">
+					Em janeiro de 2015 iniciou-se a carreira do Dr. Lucilo Fonseca Neto no escritório, atuando ao lado
+					de seu pai, o advogado Dr.Ricardo Calil.
+				</p>
+			</div>
+			<div class="flex flex-col">
+				<h3 class="text-xl font-semibold tracking-wide">Conquistas</h3>
+				<time class="text-xs tracking-wide uppercase text-coolGray-600">Jan 2016</time>
+				<p class="mt-3">
+					Durante a caminhada de Dr. Ricardo Calil na advocacia, recebeu a honrosa missão de ser o presidente
+					a OAB subseção de Itaberaí, no período de 2016 a 2018. Uma conquista que lhe trouxe muitas
+					experiências.
+				</p>
 			</div>
 		</div>
 	</section>
