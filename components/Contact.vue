23fcb89d00400798da6f4f9e778b9ad16240f048
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-07-17 20:02:10 -0300

    feat: update contact form with responsiveness

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,167 +1,163 @@
 <template>
-	<section class="body-font relative">
-		<form class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-			<div
-				class="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative"
-			>
-				<iframe
-					width="100%"
-					height="100%"
-					class="absolute inset-0"
-					frameborder="0"
-					title="map"
-					marginheight="0"
-					marginwidth="0"
-					scrolling="no"
-					loading="lazy"
-					src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7669.849964307218!2d-49.8079523!3d-16.0174202!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5f2f774f3b2d9abc!2sRicardo%20Calil%20%26%20Advogados%20Associados!5e0!3m2!1spt-BR!2sbr!4v1636910106771!5m2!1spt-BR!2sbr"
-					style="filter: grayscale(1) contrast(1.2) opacity(0.65)"
-				></iframe>
-				<div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-					<div class="lg:w-1/2 px-6">
-						<h2 class="title-font font-semibold tracking-widest text-xs">ENDEREÇO</h2>
-						<p class="mt-1" v-text="street"></p>
-					</div>
-					<div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
-						<h2 class="title-font font-semibold tracking-widest text-xs">EMAIL</h2>
-						<a class="leading-relaxed" :href="'mailto:' + social.email" v-text="social.email"></a>
-						<h2 class="title-font font-semibold tracking-widest text-xs mt-4">TELEFONE</h2>
-						<a
-							class="leading-relaxed"
-							:href="'tel:' + social.phone"
-							rel="noopener noreferrer"
-							v-text="social.phone"
-						></a>
-					</div>
+	<section class="px-8 py-12 lg:grid lg:grid-cols-12 lg:px-12 lg:gap-10">
+		<div class="flex flex-col relative lg:col-span-6">
+			<div class="bg-white flex flex-wrap py-6 rounded shadow-md mb-10 lg:order-2 lg:mb-16 lg:z-10">
+				<div class="px-6 lg:w-1/2">
+					<h2 class="title-font font-semibold tracking-widest text-xs">ENDEREÇO</h2>
+					<p class="mt-1" v-text="street"></p>
+				</div>
+				<div class="px-6 mt-4 lg:w-1/2 lg:mt-0">
+					<h2 class="title-font font-semibold tracking-widest text-xs">EMAIL</h2>
+					<a class="leading-relaxed underline" :href="'mailto:' + social.email" v-text="social.email"></a>
+					<h2 class="title-font font-semibold tracking-widest text-xs mt-4">TELEFONE</h2>
+					<a
+						class="leading-relaxed underline"
+						:href="'tel:' + social.phone"
+						rel="noopener noreferrer"
+						v-text="social.phone"
+					></a>
 				</div>
 			</div>
-			<div class="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-				<h2 class="text-lg mb-1 font-medium title-font">Contato</h2>
-				<p class="leading-relaxed mb-5">Converse agora com um advogado</p>
-				<div class="relative mb-4">
-					<label for="name" class="leading-7 text-sm">Nome</label>
-					<input
-						v-model="contactForm.name"
-						type="text"
-						name="name"
-						class="
-							w-full
-							bg-white
-							rounded
-							border
-							focus:ring-2
-							text-base
-							outline-none
-							text-gray-700
-							py-1
-							px-3
-							leading-8
-							transition-colors
-							duration-200
-							ease-in-out
-						"
-						placeholder="Nome"
-					/>
-				</div>
-				<div class="hidden relative mb-4">
-					<label for="surname" class="leading-7 text-sm">Sobrenome</label>
-					<input
-						v-model="contactForm.surname"
-						type="text"
-						name="surname"
-						class="
-							w-full
-							bg-white
-							rounded
-							border
-							focus:ring-2
-							text-base
-							outline-none
-							text-gray-700
-							py-1
-							px-3
-							leading-8
-							transition-colors
-							duration-200
-							ease-in-out
-						"
-						placeholder="Nome"
-					/>
-				</div>
-				<div class="relative mb-4">
-					<label for="phone" class="leading-7 text-sm">Telefone</label>
-					<input
-						v-model="contactForm.phone"
-						type="tel"
-						name="phone"
-						class="
-							w-full
-							rounded
-							border
-							focus:ring-2
-							text-base
-							outline-none
-							py-1
-							px-3
-							leading-8
-							transition-colors
-							duration-200
-							ease-in-out
-						"
-						placeholder="(99) 99999-9999"
-					/>
-				</div>
-				<div class="relative mb-4">
-					<label for="email" class="leading-7 text-sm">Email</label>
-					<input
-						v-model="contactForm.email"
-						type="email"
-						name="email"
-						class="
-							w-full
-							rounded
-							border
-							focus:ring-2
-							text-base
-							outline-none
-							py-1
-							px-3
-							leading-8
-							transition-colors
-							duration-200
-							ease-in-out
-						"
-						placeholder="nome@gmail.com"
-					/>
-				</div>
-				<div class="relative mb-4">
-					<label for="message" class="leading-7 text-sm">Mensagem</label>
-					<textarea
-						v-model="contactForm.message"
-						name="message"
-						class="
-							w-full
-							rounded
-							border
-							focus:ring-2
-							h-32
-							text-base
-							outline-none
-							py-1
-							px-3
-							resize-none
-							leading-6
-							transition-colors
-							duration-200
-							ease-in-out
-						"
-						placeholder="Gostaria de saber mais sobre ..."
-					></textarea>
-				</div>
-				<button class="border-0 py-2 px-6 focus:outline-none rounded text-lg" @click="submitContactForm">
-					Enviar
-				</button>
+			<iframe
+				width="100%"
+				height="100%"
+				class="rounded h-60 lg:h-full"
+				frameborder="0"
+				title="map"
+				marginheight="0"
+				marginwidth="0"
+				scrolling="no"
+				loading="lazy"
+				src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7669.849964307218!2d-49.8079523!3d-16.0174202!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5f2f774f3b2d9abc!2sRicardo%20Calil%20%26%20Advogados%20Associados!5e0!3m2!1spt-BR!2sbr!4v1636910106771!5m2!1spt-BR!2sbr"
+				style="filter: grayscale(1) contrast(1.2) opacity(0.65)"
+			></iframe>
+		</div>
+		<form class="hidden lg:col-span-6 lg:order-1 lg:flex lg:flex-col">
+			<h1 class="text-4xl font-bold text-gray-800 mb-1">Contato</h1>
+			<p class="mb-5">Converse com um advogado</p>
+			<div class="relative mb-4">
+				<label for="name" class="leading-7 text-sm">Nome</label>
+				<input
+					v-model="contactForm.name"
+					type="text"
+					name="name"
+					class="
+						w-full
+						bg-white
+						rounded
+						border
+						focus:ring-2
+						text-base
+						outline-none
+						text-gray-700
+						py-1
+						px-3
+						leading-8
+						transition-colors
+						duration-200
+						ease-in-out
+					"
+					placeholder="Nome"
+				/>
 			</div>
+			<div class="hidden relative mb-4">
+				<label for="surname" class="leading-7 text-sm">Sobrenome</label>
+				<input
+					v-model="contactForm.surname"
+					type="text"
+					name="surname"
+					class="
+						w-full
+						bg-white
+						rounded
+						border
+						focus:ring-2
+						text-base
+						outline-none
+						text-gray-700
+						py-1
+						px-3
+						leading-8
+						transition-colors
+						duration-200
+						ease-in-out
+					"
+					placeholder="Nome"
+				/>
+			</div>
+			<div class="relative mb-4">
+				<label for="phone" class="leading-7 text-sm">Telefone</label>
+				<input
+					v-model="contactForm.phone"
+					type="tel"
+					name="phone"
+					class="
+						w-full
+						rounded
+						border
+						focus:ring-2
+						text-base
+						outline-none
+						py-1
+						px-3
+						leading-8
+						transition-colors
+						duration-200
+						ease-in-out
+					"
+					placeholder="(99) 99999-9999"
+				/>
+			</div>
+			<div class="relative mb-4">
+				<label for="email" class="leading-7 text-sm">Email</label>
+				<input
+					v-model="contactForm.email"
+					type="email"
+					name="email"
+					class="
+						w-full
+						rounded
+						border
+						focus:ring-2
+						text-base
+						outline-none
+						py-1
+						px-3
+						leading-8
+						transition-colors
+						duration-200
+						ease-in-out
+					"
+					placeholder="nome@gmail.com"
+				/>
+			</div>
+			<div class="relative mb-4">
+				<label for="message" class="leading-7 text-sm">Mensagem</label>
+				<textarea
+					v-model="contactForm.message"
+					name="message"
+					class="
+						w-full
+						rounded
+						border
+						focus:ring-2
+						h-32
+						text-base
+						outline-none
+						py-1
+						px-3
+						resize-none
+						leading-6
+						transition-colors
+						duration-200
+						ease-in-out
+					"
+					placeholder="Gostaria de saber mais sobre ..."
+				></textarea>
+			</div>
+			<button class="text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 py-2 px-8 focus:outline-none rounded text-lg self-end w-48" @click="submitContactForm">
+				Enviar
+			</button>
 		</form>
 	</section>
 </template>
@@ -191,7 +187,7 @@ export default Vue.extend({
 		async submitContactForm(e: Event) {
 			e.preventDefault();
 			try {
-				const response = await fetch('https://usebasin.com/f/e40dbe79fbce', {
+				const response = await fetch(this.$config.contactFormEndpoint, {
 					method: 'POST',
 					headers: {
 						'Accept': 'application/json',
