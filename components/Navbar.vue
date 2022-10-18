093d02494a0424932c4b2a3fea55a24a50cf3222
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-10-18 15:07:28 -0300

    chore: update hero and navbar colors

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,17 @@
 <template>
 	<nav id="navbar" class="bg-white shadow h-navbar">
-		<div class="max-w-screen-xl px-4 h-full mx-auto md:flex md:justify-between md:items-center lg:px-12">
+		<div
+			class="max-w-screen-xl px-4 h-full mx-auto select-none md:flex md:justify-between md:items-center lg:px-12"
+		>
 			<div class="flex justify-between h-full">
 				<Logo></Logo>
 				<button
 					type="button"
-					class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400 md:hidden"
+					class="text-gray-700 hover:text-primary-400 active:text-primary focus:outline-hidden md:hidden"
 					aria-label="Alternar menu lateral"
 					@click="toggleSidebarStatus"
 				>
-					<img class="h-6 w-6" :src="currentMenuIcon" alt="Alternar sidebar" />
+					<img draggable="false" class="h-6 w-6" :src="currentMenuIcon" alt="Alternar sidebar" />
 				</button>
 			</div>
 
@@ -18,7 +20,8 @@
 					<NuxtLink
 						v-for="section in sections"
 						:key="section.target"
-						class="my-1 text-sm font-medium text-gray-700 hover:text-blue-500 md:my-0 lg:text-base"
+						draggable="false"
+						class="my-1 text-sm font-medium text-gray-700 hover:text-primary-400 active:text-primary md:my-0 lg:text-base"
 						:to="section.target"
 						v-text="section.title"
 					></NuxtLink>
