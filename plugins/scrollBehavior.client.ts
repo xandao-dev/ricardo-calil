191069f8727e309f1461c9f7f35524b845c7b993
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-11-09 19:57:34 -0300

    fix: scroll behavior

--- plugins/scrollBehavior.client.ts
+++ plugins/scrollBehavior.client.ts
@@ -1,10 +1,11 @@
-let navbarHeight = null;
-export default async function (to, from, savedPosition) {
+import { RouterScrollBehavior } from 'vue-router';
+
+const scrollBehavior: RouterScrollBehavior = async (to, from, savedPosition) => {
 	if (savedPosition) {
 		return savedPosition;
 	}
 
-	const findEl = async (hash, x = 0) => {
+	const findEl = async (hash: any, x = 0) => {
 		return (
 			document.querySelector(hash) ||
 			new Promise((resolve) => {
@@ -19,15 +20,23 @@ export default async function (to, from, savedPosition) {
 	};
 
 	if (to.hash) {
-		navbarHeight = navbarHeight || (await findEl('#navbar')).offsetHeight;
+		let navbarHeight = (await findEl('#navbar')).offsetHeight || 0;
 		const el = await findEl(to.hash);
 		const scrollable = document.querySelector('main') || window;
 		if ('scrollBehavior' in document.documentElement.style) {
-			return scrollable.scrollTo({ top: el.offsetTop - navbarHeight, behavior: 'smooth' });
+			return scrollable.scrollTo({
+				top: el.offsetTop - navbarHeight,
+				left: 0,
+				behavior: 'smooth',
+			});
 		} else {
 			return scrollable.scrollTo(0, el.offsetTop - navbarHeight);
 		}
 	}
 
-	return { x: 0, y: 0 };
-}
+	return { left: 0, top: 0, behavior: 'smooth' };
+};
+
+export default defineNuxtPlugin((nuxtApp) => {
+	nuxtApp.$router.options.scrollBehavior = scrollBehavior;
+});
