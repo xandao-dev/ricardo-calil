157ca9e50821b9e95ff2651acd720e813753b280
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-11-10 00:12:38 -0300

    chore: add typecheck module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     ssr: false,
-    // target: 'static',
-
     runtimeConfig: {
         public: {
             dev: process.env.NODE_ENV !== 'production',
