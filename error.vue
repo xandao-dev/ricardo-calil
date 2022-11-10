ceff63272d2a226f9163531e85fedabb074f4813
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-11-09 23:47:33 -0300

    chore: improve error page

--- error.vue
+++ error.vue
@@ -1,8 +1,12 @@
 <script lang="ts" setup>
 interface IError {
     statusCode: number;
+    message: string;
 }
-defineProps<{ error: IError }>();
+const props = defineProps<{ error: IError }>();
+console.error(props.error.message);
+
+const handleError = () => clearError({ redirect: '/' });
 </script>
 
 <template>
@@ -11,38 +15,38 @@ defineProps<{ error: IError }>();
         class="flex flex-col items-center justify-center px-8 py-12 text-center lg:px-12"
     >
         <h2 class="mb-12 text-9xl font-extrabold"><span class="sr-only">Erro</span>404</h2>
-        <p class="mb-12 text-2xl font-semibold md:text-3xl">Desculpe, não encontramos essa página</p>
-        <NuxtLink draggable="false" to="/" class="w-full sm:w-auto">
-            <button
-                class="mx-auto w-full select-none rounded bg-primary py-3 font-semibold text-white hover:bg-primary-700 focus:outline-none active:bg-primary-800 sm:px-24 sm:py-4 md:text-xl"
-            >
-                Início
-            </button>
-        </NuxtLink>
+        <p class="mb-12 text-2xl font-semibold md:text-3xl">Desculpe, não encontramos esta página</p>
+        <button
+            class="mx-auto w-full select-none rounded bg-primary py-3 font-semibold text-white hover:bg-primary-700 focus:outline-none active:bg-primary-800 sm:w-auto sm:px-24 sm:py-4 md:text-xl"
+            @click="handleError"
+        >
+            Início
+        </button>
     </section>
     <section v-else class="flex flex-col items-center justify-center px-8 py-12 text-center lg:px-12">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="mb-12 h-40 w-40">
-            <path
-                fill="currentColor"
-                d="M256,16C123.452,16,16,123.452,16,256S123.452,496,256,496,496,388.548,496,256,388.548,16,256,16ZM403.078,403.078a207.253,207.253,0,1,1,44.589-66.125A207.332,207.332,0,0,1,403.078,403.078Z"
-            ></path>
-            <rect width="176" height="32" x="168" y="320" fill="currentColor"></rect>
-            <polygon
-                fill="currentColor"
-                points="210.63 228.042 186.588 206.671 207.958 182.63 184.042 161.37 162.671 185.412 138.63 164.042 117.37 187.958 141.412 209.329 120.042 233.37 143.958 254.63 165.329 230.588 189.37 251.958 210.63 228.042"
-            ></polygon>
-            <polygon
-                fill="currentColor"
-                points="383.958 182.63 360.042 161.37 338.671 185.412 314.63 164.042 293.37 187.958 317.412 209.329 296.042 233.37 319.958 254.63 341.329 230.588 365.37 251.958 386.63 228.042 362.588 206.671 383.958 182.63"
-            ></polygon>
-        </svg>
-        <p class="mb-12 text-2xl font-semibold md:text-3xl">Desculpe, ocorreu um erro</p>
-        <NuxtLink draggable="false" to="/" class="w-full sm:w-auto">
-            <button
-                class="mx-auto w-full select-none rounded bg-primary py-3 font-semibold text-white hover:bg-primary-700 focus:outline-none active:bg-primary-800 sm:px-24 sm:py-4 md:text-xl"
-            >
-                Início
-            </button>
-        </NuxtLink>
+        <h2 class="mb-12 text-9xl font-extrabold"><span class="sr-only">Erro</span></h2>
+        <p class="mb-12 text-2xl font-semibold md:text-3xl">Desculpe, esta página está indisponível</p>
+        <button
+            class="mx-auto w-full select-none rounded bg-primary py-3 font-semibold text-white hover:bg-primary-700 focus:outline-none active:bg-primary-800 sm:w-auto sm:px-24 sm:py-4 md:text-xl"
+            @click="handleError"
+        >
+            Início
+        </button>
     </section>
 </template>
+
+<style scoped>
+section::before {
+    content: '';
+    pointer-events: none;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    background-image: url('/textures/grainy.png');
+    background-repeat: repeat;
+    opacity: 0.1;
+}
+</style>
